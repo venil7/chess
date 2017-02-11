8ca9bafe0cdeee1bd44fd3137a56be0b50460c93
Art Deineka <art.deineka@darkruby.co.uk>
2017-02-11 22:13:23 +0000

    possible moves and tests for bishop

--- src/pieces/bishop.ts
+++ src/pieces/bishop.ts
@@ -1,9 +1,22 @@
 import { Piece, IPiece, Color } from './piece';
+import { Coordinates } from '../coordinates';
+import Board from '../board';
 
 export default class Bishop extends Piece implements IPiece {
   get weight(): number { return 2; }
+
   toString(): string { return this.color == Color.white ? '♗' : '♝' };
+
   clone(): IPiece {
     return new Bishop(this.color);
+  }
+
+  public possibleMoves(coordinates: Coordinates, board: Board) {
+    return [
+      ...this.possiblePathMoves(coordinates.upleftPath(), board),
+      ...this.possiblePathMoves(coordinates.uprightPath(), board),
+      ...this.possiblePathMoves(coordinates.downleftPath(), board),
+      ...this.possiblePathMoves(coordinates.downrightPath(), board),
+    ];
   }
 }
